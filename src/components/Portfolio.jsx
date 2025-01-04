efc0580e8f06ac9cbe9045f63a1c620a2fd458e0
Kimberley Hoang <kimberley.hoang528@gmail.com>
2025-01-04 02:39:21 -0500

    added projects to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,12 +4,36 @@ export default function Portfolio()
     <section id="portfolio">
         <h1>Portfolio</h1>
         <blockquote>| Hey Ferb, I know what we're gonna do today.</blockquote>
-        <h3>UW Rocketry - Controlled Canards</h3>
-        <h3>Simulated Mobile Navigation Robot</h3>
-        <h3>WaterLook</h3>
-        <h3>Facial Detection</h3>
-        <p>Mental Note</p>
-        <p>Hop To It</p>
+
+        <h3>Waterloo Rocketry - Rocket Canards</h3>
+        <p>
+            Helping code the real time operating system to control canards that reduce rocket roll rate. 
+        </p>
+        <p>
+            Learning about active control systems with cool people at Waterloo Rocketry.
+        </p>
+        <h3>NavBot - Mobile Robot Simulation</h3>
+        <p>
+            Using ROS and Gazebo to simulate a mobile robot that uses lidar and camera sensors.
+        </p>
+        <h3>Waterlook - Automated Irrigation Systems</h3>
+        <p>
+            Fine tuning ML models for crop watering times based on envirionmental conditions for water conservation.
+        </p>
+        <p>
+           Guided by data scientists and machine learning engineers at RBC Borealis as a part of their Let's SOLVE It program.
+        </p>
+        <h3>Fallacy - Facial Expression Detection</h3>
+        <p>
+            Leveraging Pytorch and YOLOv5 models for facial expression detection.
+        </p>
+        <h3>Mental Note - Image Sentiment Journal</h3>
+        <p>
+            Developing a sentiment journal that used sentiment analysis and neural style transfer to create images based on user emotions.
+        </p>
+        <p>
+            Awarded at the University of Toronto's Hack the MIST hackathon.
+        </p>
     </section>
         
     </>
