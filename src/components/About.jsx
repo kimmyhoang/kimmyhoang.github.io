ff0d0e71ec1b7ffa792584cee658ab689e1560f7
Kimberley Hoang <kimberley.hoang528@gmail.com>
2025-01-04 02:13:32 -0500

    fix typo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ export default function About()
     return <>
         <section id="about">
             <p>
-            ""When you've finished your own toilet in the morning, then it is time to attend to the toilet of your planet, just so, with the greatest care."  - The Little Prince
+            "When you've finished your own toilet in the morning, then it is time to attend to the toilet of your planet, just so, with the greatest care."  - The Little Prince
             </p>
             <div className="contact">
                 <a href="https://www.github.com/kimmyhoang">Github</a>
