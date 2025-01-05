a485dfe4588afd031e60cf27d1af87e3bbd455f7
Kimberley Hoang <kimberley.hoang528@gmail.com>
2025-01-05 18:45:06 -0500

    fix mobile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,13 @@ export default function About()
 {
     return <>
         <section id="about">
+            <div className="quote">
             <p>
             "When you've finished your own toilet in the morning, <br class="non-mobile-break" /> then it is time to attend to the toilet of your planet,
             just so, with the greatest care."  
             <br/>- The Little Prince by Antoine de Saint-Exupéry
-
             </p>
+            </div>
             <div className="contact">
                 <a href="https://www.github.com/kimmyhoang">Github</a>
                 <a href="https://www.linkedin.com/in/kimmyhoang">Linkedin</a>
